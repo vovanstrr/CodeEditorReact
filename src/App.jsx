dd7a7549ea319bf223f9d96ea3055ddef2ef0d05
Vladimir Staroverov <vstarover@gmail.com>
2025-01-09 00:28:01 +0300

    исправление ошибок

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,77 @@
-import { useState } from 'react'
-import Select from 'react-select'
-import './App.css';
-import Output from './Output';
-import ThemeContext from './MyContext'
-import CodeEditor from './CodeEditor';
-import { executeCode } from './api'
+import { useState } from "react";
+import Select from "react-select";
+import "./App.css";
+import Output from "./Output";
+import ThemeContext from "./MyContext";
+import CodeEditor from "./CodeEditor";
+import { executeCode } from "./api";
 import { CODE_SNIPPETS } from "./constants";
-
-// import { Console, Hook, Unhook } from 'console-feed'
-
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [selectValue, setSelectValue] = useState('javascript')
-  const [code, setCode] = useState("console.log('Hello world');\nconsole.log('Hello world');\nconsole.log('Hello world');");
-  const [result, setResult] = useState('')
-
+  const [selectValue, setSelectValue] = useState("javascript");
+  const [code, setCode] = useState(
+    "console.log('Hello world');\nconsole.log('Hello world');\nconsole.log('Hello world');"
+  );
+  const [result, setResult] = useState("");
 
   const options = [
-    { value: 'javascript', label: 'Javascript' },
-    { value: 'python', label: 'Python' },
-  ]
+    { value: "javascript", label: "Javascript" },
+    { value: "python", label: "Python" },
+  ];
 
   const clickRun = async () => {
-
     if (!code) return;
     try {
       const response = await executeCode(selectValue, code);
-      console.log('selectValue ', selectValue);
+      console.log("selectValue ", selectValue);
 
-      console.log('response ', response);
-      console.log('resp output ', response.output)
-      setResult(response.output)
+      console.log("response ", response);
+      console.log("resp output ", response.output);
+      setResult(response.output);
       console.log(result);
-
     } catch (e) {
       console.log(e);
     }
 
     // console.log('code', code);
-    console.log(selectValue == 'Javascript' ? "JS" : "python");
+    console.log(selectValue == "Javascript" ? "JS" : "python");
     // console.log(selectValue)
-  }
+  };
 
   const changeHandler = (e) => {
     // console.log(e.value);
-    setSelectValue(e.value)
-    setCode(CODE_SNIPPETS[e.value])
-  }
+    setSelectValue(e.value);
+    setCode(CODE_SNIPPETS[e.value]);
+  };
 
   return (
     <ThemeContext.Provider value={{ selectValue, code, setCode, result }}>
-      <div className='app'>
+      <div className="app">
         <div className="description">
           <h1>Описание задачи</h1>
           Вывести Hello World 3 раза
         </div>
-
-        <div className='editor'>
-          <div className='select'>
-            <Select options={options} defaultValue={options[0]} onChange={changeHandler} />
-            <button onClick={clickRun}>
-              Run
-            </button>
-            <CodeEditor />
+        <div className="container">
+          <div className="select">
+            <Select
+              options={options}
+              defaultValue={options[0]}
+              onChange={changeHandler}
+            />
+            <button onClick={clickRun}>Run</button>
           </div>
-          <div className="result">
-            <Output />
+
+          <div className="editor">
+            <CodeEditor />
+            <div className="result">
+              <Output />
+            </div>
           </div>
         </div>
-
       </div>
     </ThemeContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/CodeEditor.jsx
+++ src/CodeEditor.jsx
@@ -1,30 +1,30 @@
-import React, { useContext } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-import { python } from '@codemirror/lang-python'
-import { tokyoNight  } from '@uiw/codemirror-theme-tokyo-night'
-import ThemeContext from './MyContext';
+import React, { useContext } from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
+import { tokyoNight } from "@uiw/codemirror-theme-tokyo-night";
+import ThemeContext from "./MyContext";
 
 function CodeEditor() {
-  const {selectValue, code, setCode} = useContext(ThemeContext)
+  const { selectValue, code, setCode } = useContext(ThemeContext);
   // const [value, setValue] = React.useState("console.log('hello world!');");
   const onChange = React.useCallback((val, viewUpdate) => {
-    console.log('val:', val);
- 
-    
+    console.log("val:", val);
+
     setCode(val);
   }, []);
-//[javascript({ jsx: true })] [python()]
-  console.log('codeEditor ', selectValue == 1 ? "JS" : "python");
-  
+  //[javascript({ jsx: true })] [python()]
+  console.log("codeEditor ", selectValue == "javascript" ? "JS" : "python");
+
   return (
-     <CodeMirror 
-  value={code} 
-  height="300px" 
-  extensions={selectValue == 1 ? [javascript({ jsx: true })] :  [python()]} 
-  onChange={onChange} 
-  theme={tokyoNight }
-  />
-)
+    <CodeMirror
+      value={code}
+      height="300px"
+      extensions={ selectValue == "javascript" ? [javascript({ jsx: true })] : [python()] }
+      onChange={onChange}
+      theme={tokyoNight}
+    />
+  );
 }
+
 export default CodeEditor;
